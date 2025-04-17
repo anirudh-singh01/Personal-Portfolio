69c505bcd4a7acc1af4e2bd6e94f132781398a8d
anirudh-singh01 <anirudhsingh.ani321@gmail.com>
2025-04-18 00:04:17 +0530

    fix

--- script.js
+++ script.js
@@ -50,9 +50,7 @@ document.getElementById('contact-form').addEventListener('submit', async functio
     }
 });
 
-/**
- * 
- * 
+/*
  * Function to Show Pop-up Notification
  */
 function showPopup(message, type) {
